37d45e24bbc6fd2a1541286609a9ee86eabd9a1b
chanceyang <chanceyang@datastory.com.cn>
2020-01-09 15:50:19 +0800

    slight modification

--- stopService.sh
+++ stopService.sh
@@ -3,7 +3,7 @@
 echo "stopping XXXXXX.."
 
 #这里如果只用JarLauncher,有误杀其他作业的风险,通过选项来识别
-for i in `jps -lv | grep "JarLauncher" | grep "ds-hermes-sched-lib" | awk '{print $1}'`
+for i in `jps -lv | grep "JarLauncher" | grep "ds-XXXXXX-sched-lib" | awk '{print $1}'`
 do
     echo "killing pid  "${i} ;
     kill ${i}
